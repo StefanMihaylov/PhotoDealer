4484dfbdbf2bc4e889624b8fa47f4e10e1691718
StefanMihaylov <mihaylov.s@gmail.com>
2014-11-08 15:34:10 +0200

    new Bootstrap theme added

--- Source/Web/PhotoDealer.Web/App_Start/BundleConfig.cs
+++ Source/Web/PhotoDealer.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace PhotoDealer.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.slate.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
